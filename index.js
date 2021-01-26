6879041bcdf4f9fb141ef2c3c1df8de9cdedde7f
hoony443 <37019259+hoony443@users.noreply.github.com>
2021-01-26 21:26:21 +0900

    js update

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const faqQustion1 = document.querySelector(".faq-qustion--1");
-const faqAnswer1 = document.querySelector(".faq-answer--1");
-const faqQustion2 = document.querySelector(".faq-qustion--2");
-const faqAnswer2 = document.querySelector(".faq-answer--2");
-const faqQustion3 = document.querySelector(".faq-qustion--3");
-const faqAnswer3 = document.querySelector(".faq-answer--3");
+const faqQustion = document.querySelectorAll(".faq-qustion");
+const faqAnswer = document.querySelectorAll(".faq-answer");
 
+for (let i = 0; i < faqQustion.length; i++) {
+    faqQustion[i].addEventListener("click", openfunc);
 
-function openAnswer1() {
-    console.log(faqAnswer1);
-    if (faqAnswer1.classList.contains("hidden")) {
-        faqAnswer1.classList.replace("hidden", "open");
-    } else {
-        faqAnswer1.classList.replace("open", "hidden");
+    function openfunc() {
+        if (faqAnswer[i].classList.contains("hidden")) {
+            faqAnswer[i].classList.replace("hidden", "open");
+        } else {
+            faqAnswer[i].classList.replace("open", "hidden");
+        }
     }
 }
-function openAnswer2() {
-    console.log(faqAnswer2);
-    if (faqAnswer2.classList.contains("hidden")) {
-        faqAnswer2.classList.replace("hidden", "open");
-    } else {
-        faqAnswer2.classList.replace("open", "hidden");
-    }
-}
-function openAnswer3() {
-    console.log(faqAnswer3);
-    if (faqAnswer3.classList.contains("hidden")) {
-        faqAnswer3.classList.replace("hidden", "open");
-    } else {
-        faqAnswer3.classList.replace("open", "hidden");
-    }
-}
-
-faqQustion1.addEventListener("click", openAnswer1);
-faqQustion2.addEventListener("click", openAnswer2);
-faqQustion3.addEventListener("click", openAnswer3); 
